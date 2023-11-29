a983a382153438b473085ce6713239775be941d9
iamsyed21 <syedmoinahmed01@gmail.com>
2023-11-29 21:47:51 +0530

    changes

--- index.js
+++ index.js
@@ -44,15 +44,8 @@ app.use("/posts", postRoutes);
 const PORT = process.env.PORT || 3000;
 mongoose.set('strictQuery', true);
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URL)
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-
-    /* ADD DATA ONE TIME */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
